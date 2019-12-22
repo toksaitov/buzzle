b3fc83b60c893a04466dcf9e0eafee454b8611cf
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-12-22 20:02:14 +0600

    Prepare data models (Closes #3)
    - [x] Setup an ORM library
    - [x] Add the DB connection logic
    - [x] Define the DB's schema with ORM models
    - [x] Define DB's relations
    - [x] Add ORM and DB syncing
    - [x] Add logic to populate DB with initial data
    - [x] Add password hashing with crypt

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 const express = require('express')
+const sequelize = require('sequelize')
+const bcrypt = require('bcryptjs')
 require('dotenv').config()
+
+const database = new Sequelize({
+    'host': process.env.BUZZLE_DB_HOST,
+    'port': process.env.BUZZLE_DB_PORT,
+    'database': process.env.BUZZLE_DB_NAME,
+    'username': process.env.BUZZLE_DB_USER,
+    'password': process.env.BUZZLE_DB_PASSWORD,
+    'dialect': process.env.BUZZLE_DB_DIALECT
+})
+
+const User = database.define('user', {
+    'login': {
+        'type': Sequelize.STRING,
+        'allowNull': false,
+        'unique': true
+    },
+    'password': {
+        'type': Sequelize.STRING,
+        'allowNull': false
+    },
+    'administrator': {
+        'type': Sequelize.BOOLEAN,
+        'allowNull': false,
+        'defaultValue': false
+    }
+})
+
+const Message = database.define('Message', {
+    'content': {
+        'type': Sequelize.STRING,
+        'allowNull': false
+    }
+})
+
+User.hasMany(Message)
+Message.belongsTo(User)
 
 const app = express()
 app.use(express.static('public'))
@@ -16,6 +54,14 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register')
+});
+
+sequelize.sync().then(() => User.upsert({
+    'login': process.env.BUZZLE_ADMIN_USER,
+    'password': bcrypt.hashSync(proces.env.BUZZLE_ADMIN_PASS,
+                                proces.env.BUZZLE_HASHING_ROUNDS)
+})).then(() => {
+    app.listen(port, () => console.log(`Buzzle is listening on port ${port}!`))
 });
 
 const port = process.env.BUZZLE_PORT
