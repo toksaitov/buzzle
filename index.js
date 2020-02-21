b32e05f8e737b7b47c27c964d4aa4e140854f796
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-02-21 15:07:25 +0000

    Add messaging logic (Closes #5)
    - [x] Add message creation handlers for the POST request
    - [x] Add error checking logic for message creation
    - [x] Improve the index page route to query and list the messages
    - [x] Add removal logic for messages
    - [x] Add editing logic for messages

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import parameters from './parameters.js'
-import database from './database.js'
+import parameters from './lib/parameters.js'
+import database from './lib/database.js'
 
-import serverBuilder from './server.js'
+import serverBuilder from './lib/server.js'
 const server = serverBuilder(parameters);
 
-import users from './users.js'
+import users from './lib/users.js'
 users(parameters, server, database);
 
+import messages from './lib/messages.js'
+messages(parameters, server, database);
+
 server.get('/', (req, res) => {
-    res.render('index', { 'session': req.session })
+    const Message = database.models.Message;
+    const User = database.models.User;
+    Message.findAll({
+        'include': [{
+            'model': User
+        }]
+    }).then(messages => {
+        res.render('index', { messages, 'session': req.session });
+    }).catch(error => {
+        console.error(error);
+        res.status(503).end('Service Unavailable');
+    });
 });
 
 database.start().then(() => {
