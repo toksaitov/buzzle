2c874c36d636214e629eaafef786be484ab6a5bb
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-03-30 21:02:38 +0600

    Add snapshot tests (Closes #15)

--- buzzle-front/src/components/Error.js
+++ buzzle-front/src/components/Error.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Error(props) {
-    const error = props.error;
+    const error = props.error || "Something went wrong...";
 
     return (
         <div className="row justify-content-center mt-3">
-            <div className="col-md-4">
+            <div className="col-md-6">
                 <div className="alert alert-danger">{error}</div>
             </div>
         </div>
--- buzzle-front/src/components/Error.test.js
+++ buzzle-front/src/components/Error.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Error from './Error.js';
 
@@ -9,7 +10,19 @@ it('renders without crashing', () => {
     ReactDOM.render(<Error />, div);
 });
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<Error error="Something went wrong..."/>);
+it('renders without crashing with error', () => {
+  const wrapper = shallow(<Error error="Something went wrong..." />);
   expect(wrapper.contains("Something went wrong...")).toBe(true);
+});
+
+it('renders without crashing with empty error', () => {
+  const wrapper = shallow(<Error error="" />);
+  expect(wrapper.contains("Something went wrong...")).toBe(true);
+});
+
+it('renders correctly and equal to snapshot', () => {
+  const tree = renderer.create(<Error error="Something went wrong..." />)
+                       .toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
