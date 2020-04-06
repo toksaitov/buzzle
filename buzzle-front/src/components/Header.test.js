fbccc13cc5228a8b9ba8b5e8fc12a901a26185d6
Erkinbek <erkinbek.abdurahman2@gmail.com>
2020-04-06 21:25:56 +0600

    Single test for header component

--- buzzle-front/src/components/Header.test.js
+++ buzzle-front/src/components/Header.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
 
 import Header from './Header.js';
 
@@ -9,12 +8,3 @@ it('renders without crashing', () => {
     ReactDOM.render(<Header />, div);
 });
 
-it('renders without crashing with error', () => {
-    const wrapper = shallow(<Header error="Something went wrong..." />);
-    expect(wrapper.contains("Something went wrong...")).toBe(true);
-});
-
-it('renders without crashing with empty error', () => {
-    const wrapper = shallow(<Header error="Something has gone completely wrong!" />);
-    expect(wrapper.contains("Something went wrong...")).toBe(true);
-});
