6f6ffca0a8314be642ef4b63f5529eb0078a5127
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-04-06 19:31:22 +0600

    Add link to the company site on footer

--- buzzle-front/src/components/Footer.js
+++ buzzle-front/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer>
             <nav className="navbar navbar-light bg-light">
-                <p className="mx-auto mt-3">&copy; 2021</p>
+                <p className="mx-auto mt-3">&copy; 2021 <a href="https://devcit.auca.kg">DEVCIT</a></p>
             </nav>
         </footer>
     );
